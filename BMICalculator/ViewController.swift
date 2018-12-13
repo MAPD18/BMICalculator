0cf47319f233ebdecfe043bcb1b8c0274f9f8d1b
Rodrigo Vieira <digovs@gmail.com>
2018-12-13 18:58:34 -0500

    Initial Commit

---
+++ BMICalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BMICalculator
+//
+//  Created by Rodrigo Silva on 2018-12-13.
+//  Copyright © 2018 Rodrigo Silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
